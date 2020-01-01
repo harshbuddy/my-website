c28acca725c27f351f77b3584596500487d4b4b7
Harsh <harshbuddy@yahoo.ca>
2020-01-01 18:49:20 -0500

    updated

---
+++ build/precache-manifest.8e24cc2e7273dc54243c263fa6b16562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7cfd04a4aeb1928997b39a7f6e0a00",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "a0d2731bed90c11654fe",
+    "url": "/my-website/static/css/main.635099e2.chunk.css"
+  },
+  {
+    "revision": "07b6c58a360db75ac848",
+    "url": "/my-website/static/js/2.61e71957.chunk.js"
+  },
+  {
+    "revision": "0258a0275a47d3474e0c0ae2865d22a9",
+    "url": "/my-website/static/js/2.61e71957.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0d2731bed90c11654fe",
+    "url": "/my-website/static/js/main.7e3dd694.chunk.js"
+  },
+  {
+    "revision": "1b9abe144448a794a4db",
+    "url": "/my-website/static/js/runtime-main.b950248c.js"
+  }
+]);
