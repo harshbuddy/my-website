806f27e9ff448bead3e5b3ef3ebb48400f09ae60
Harsh <harshbuddy@yahoo.ca>
2020-01-01 20:26:16 -0500

    new favicon

---
+++ build/precache-manifest.f05bed71618e669ea0f1473e047b21d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e7485bc069783d4e238681edc2d4fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f20867ec5b5ee946a870",
+    "url": "/static/css/main.635099e2.chunk.css"
+  },
+  {
+    "revision": "8d4d3b59122884741819",
+    "url": "/static/js/2.235bfe3a.chunk.js"
+  },
+  {
+    "revision": "0258a0275a47d3474e0c0ae2865d22a9",
+    "url": "/static/js/2.235bfe3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f20867ec5b5ee946a870",
+    "url": "/static/js/main.a6a6a427.chunk.js"
+  },
+  {
+    "revision": "f5872a621ceddbb4a534",
+    "url": "/static/js/runtime-main.cab9f41d.js"
+  }
+]);
