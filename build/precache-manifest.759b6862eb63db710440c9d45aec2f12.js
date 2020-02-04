cfbab1f5bff258262f2d3ec2801666be27e9a794
Harsh Patel <harsh@Harshs-MacBook-Pro.local>
2020-02-03 19:37:32 -0500

    updated new

---
+++ build/precache-manifest.759b6862eb63db710440c9d45aec2f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ae4779d1c2baaa86a19ba05c979bb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4922c48987ab0c7cd80e",
+    "url": "/static/css/main.2a868423.chunk.css"
+  },
+  {
+    "revision": "8d4d3b59122884741819",
+    "url": "/static/js/2.235bfe3a.chunk.js"
+  },
+  {
+    "revision": "0258a0275a47d3474e0c0ae2865d22a9",
+    "url": "/static/js/2.235bfe3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4922c48987ab0c7cd80e",
+    "url": "/static/js/main.46001398.chunk.js"
+  },
+  {
+    "revision": "f5872a621ceddbb4a534",
+    "url": "/static/js/runtime-main.cab9f41d.js"
+  }
+]);
